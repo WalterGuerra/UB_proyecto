0c7518f78a03739ab9cdf3db3644e43335cea289
Walter <walterguerrarivera2@gmail.com>
2024-10-04 01:36:35 -0500

    api 2

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="proyecto"
+        host="b3mov0t24gltgdymhcr2-mysql.services.clever-cloud.com",
+        user="upmb4w1ymjlodhzn",
+        password="jSrLN9cQYS1GVU54lSdq",
+        database="b3mov0t24gltgdymhcr2"
     )
